fd80465a840a229fa799e3c182ade27f7d94dfdf
adriaalos <adriaalos@adriaaloss-MacBook-Air.local>
2025-05-28 09:51:27 +0200

    remove motion-v-nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,6 @@ export default defineNuxtConfig({
         '@nuxt/ui',
         '@pinia/nuxt',
         '@nuxtjs/i18n',
-        'motion-v/nuxt',
         '@vueuse/nuxt',
         '@storyblok/nuxt',
         'v-gsap-nuxt',
