52557c5592bb566dd2fa34e44a202c6c6c909e4f
adriaalos <adriaalos@adriaaloss-MacBook-Air.local>
2025-06-14 13:12:13 +0200

    form tested

--- server/api/mail/index.post.ts
+++ server/api/mail/index.post.ts
@@ -6,15 +6,15 @@ export default defineEventHandler(async (event) => {
     const resend = new Resend(config.resendApiKey)
 
     const {
-        name, email, phone, company, country, region, message, customer, supplier } = await readBody(event)
+        name, surname, email, phone, company, country, region, message, customer, supplier } = await readBody(event)
 
-    try {
-        const data = await resend.emails.send({
-            from: 'Copreci (copreci.com) <home@copreci.com>',
-            to: ['home@copreci.com'],
-            subject: 'Formulario de contacto (copreci.com)',
-            html: `
+    const data = await resend.emails.send({
+        from: 'Copreci (copreci.com) <home@copreci.com>',
+        to: ['home@copreci.com', 'adriaalos@gmail.com'],
+        subject: 'Formulario de contacto (copreci.com)',
+        html: `
             <p><strong>Nombre:</strong> ${name ? name : '-'}</p>
+            <p><strong>Apellidos:</strong> ${surname ? surname : '-'}</p>
             <p><strong>Email:</strong> ${email ? email : '-'}</p>
             <p><strong>Teléfono:</strong> ${phone ? phone : '-'}</p>
             <p><strong>Empresa:</strong> ${company ? company : '-'}</p>
@@ -24,11 +24,7 @@ export default defineEventHandler(async (event) => {
             <p><strong>Cliente:</strong> ${customer == 'yes' ? 'Sí' : 'No'}</p>
             <p><strong>Proveedor:</strong> ${supplier == 'yes' ? 'Sí' : 'No'}</p>
             `,
-        })
+    })
 
-        return data
-    }
-    catch (error) {
-        return { error }
-    }
+    return data
 })
